480cdd44c7732c9c6558972af294defa02e42621
Binoy <binoyjayan@gmail.com>
2021-06-03 09:10:10 -0700

    more on rust - traits lifetimes and circular references

--- languages/rust/traits/src/main.rs
+++ languages/rust/traits/src/main.rs
@@ -1,10 +1,23 @@
+
+mod introduction;
+mod overloading;
+mod dispatches;
+mod heterogenous;
+
+use introduction::*;
+use overloading::*;
+use dispatches::*;
+use heterogenous::*;
+
 fn main() {
     println!("Traits");
-    traits_basic();
-}
-
-#[allow(dead_code)]
-#[allow(unused_variables)]
-fn traits_basic() {
-
+    // traits_basic();
+    // traits_parameters();
+    // traits_into();
+    // traits_drop();
+    // traits_operator_overloading();
+    // static_dispatch(); // compile time
+    // dynamic_dispatch_simple(); // runtime
+    // dynamic_dispatch();
+    heterogenous_vectors();
 }
